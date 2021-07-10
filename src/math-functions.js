07e5f3ce09aa84ed8f7676bf63be9572010c343b
TANNER MECK <steve.meck246@gmail.com>
2021-07-10 16:09:08 -0700

    First sum problem passing

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,9 +12,8 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    return [a + b, `The sum of ${a} and ${b} is ${a + b}.`]; 
 }
-
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
 
